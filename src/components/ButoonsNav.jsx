2f848877ab8eea98a3c1af9b8d7dca3a5753016b
Bruno M <brunomoron56@gmail.com>
2023-03-04 22:37:02 -0300

    add change of icon state

--- src/components/ButoonsNav.jsx
+++ src/components/ButoonsNav.jsx
@@ -1,7 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faBars,
+  faXmark
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function ButoonsNav() {
+  // estado que controla icono
+  const [icon, setIcon] = useState(0);
+  function handleClick() {
+  }
+  function handleIcon(){
+    // controla es estado del icono
+    if(icon === 0){
+      setIcon(1);
+    }else{
+      setIcon(0);
+    }
+  }
   return (
     <>
       <div className="flex gap-4 text-2xl mr-3 xl:hidden">
@@ -11,14 +28,25 @@ function ButoonsNav() {
             className="hover:text-3xl hover:mt-1 transition-all duration-300"
           />
         </button>
-        <button>
-          <FontAwesomeIcon icon={faBars} />
+        <button id="barraIcon" onClick={handleClick}>
+        <FontAwesomeIcon
+            icon={icon === 0 ? faBars : faXmark}
+            onClick={handleIcon}
+            className="hover:text-3xl hover:mt-1 transition-all duration-300"
+          />
         </button>
       </div>
       {/* for xl screens */}
       <div className="hidden xl:flex gap-3 mr-12 ">
-        <button className="border text-2xl pl-5 pr-5 pt-2 pb-2 font-sans font-semibold rounded-lg bg-stone-900 border-solid ">Submit a request</button>
-        <button title="Open a dialog" className=" text-2xl pl-7 pr-7 pt-2 pb-2 font-sans  rounded-lg bg-[#4C5FD5] hover:text-black hover:bg-white duration-200">Sign in</button>
+        <button className="border text-2xl pl-5 pr-5 pt-2 pb-2 font-sans font-semibold rounded-lg bg-stone-900 border-solid ">
+          Submit a request
+        </button>
+        <button
+          title="Open a dialog"
+          className=" text-2xl pl-7 pr-7 pt-2 pb-2 font-sans  rounded-lg bg-[#4C5FD5] hover:text-black hover:bg-white duration-200"
+        >
+          Sign in
+        </button>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavLinks from "./NavLinks";
 
 function Navbar() {
   return (
-    <nav className=" flex md:pl-8 md:pr-8 justify-between  items-center  bg-[#000000] text-[#fff] w-full h-20">
+    <nav id="Navegador" className=" flex md:pl-8 md:pr-8 justify-between  items-center  bg-[#000000] text-[#fff] w-full h-20">
       <NavLinks />
       <ButoonsNav />
     </nav>
