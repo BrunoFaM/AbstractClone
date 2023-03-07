e98b4cc7172928c029271cc2ff58819aea6d7cba
Bruno M <brunomoron56@gmail.com>
2023-03-07 20:03:42 -0300

    x

--- src/components/MenuDesplegable.jsx
+++ src/components/MenuDesplegable.jsx
@@ -4,7 +4,7 @@ function MenuDesplegable() {
   return (
     <div
       id="desplegable"
-      className="text-white bg-black border-t flex flex-col pt-12 pb-12 hidden w-screen"
+      className="text-white bg-black border-t flex flex-col pt-12 pb-12 w-screen"
     >
       <button className="w-max text-2xl pl-3 pr-3 pt-2 pb-4 font-sans border-b self-center">
         Submit a request
